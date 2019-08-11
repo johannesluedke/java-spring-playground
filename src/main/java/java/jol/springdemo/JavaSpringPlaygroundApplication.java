23d3137604e7b5956c44a4a2357bf7e6fc087817
Johannes Lüdke <johannes.luedke@gmail.com>
2019-08-11 09:55:03 +0200

    add initial spring initiliazer project boilerplate
    * Actuator, H2, JBD, Lombok
    * Maven-based project

---
+++ src/main/java/java/jol/springdemo/JavaSpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package java.jol.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/jol/springdemo/JavaSpringPlaygroundApplicationTests.java
@@ -0,0 +1,16 @@
+package java.jol.springdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaSpringPlaygroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
